2a1f1b8f28a9ec5edef5ad1783e295e533da6e17
Radu-Stefan Popovici <radu.popovici-2@student.manchester.ac.uk>
2023-04-09 14:31:57 +0100

    Add authentification through spotify
    Currently a simple login button. Issue #2

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -5,6 +5,26 @@ import { Routes, Route, Link } from "react-router-dom"
 export default class LoginPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            spotifyAuthenticated: false
+        };
+        this._loginButtonPressed = this._loginButtonPressed.bind(this);
+        this.autheticateSpotify = this.autheticateSpotify.bind(this);
+    }
+
+    autheticateSpotify(){
+        fetch('/spotify/is-authenticated')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                if (!data.status){
+                    fetch('/spotify/get-auth-url')
+                        .then((response) => response.json())
+                        .then((data) => {
+                            window.location.replace(data.url);
+                        });
+                }
+            });
     }
 
     render() {
@@ -12,10 +32,13 @@ export default class LoginPage extends Component {
             <div class="login-div">
                 <h2>Discover your new favorite music!</h2>
                 <p>Sign in using Spotify</p>
-                <Link to="/">
-                    <button type="button" class="btn btn-success" id="login-btn">Spotify</button>
-                </Link>
+                <button type="button" class="btn btn-success" id="login-btn" onClick={this._loginButtonPressed}>Spotify</button>
             </div>
             );
+    }
+
+    _loginButtonPressed() {
+        this.autheticateSpotify();
+        console.log(this.state.spotifyAuthenticated);
     }
 }
---
+++ spotify/credentials.py
@@ -0,0 +1,4 @@
+CLIENT_ID = "085e30b9ed0f4146bc94f5db722f0fab"
+CLIENT_SECRET = "8377e03694564a8ea18d8da913a05653"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+ACCESS_TOKEN_URL = "https://accounts.spotify.com/api/token"
